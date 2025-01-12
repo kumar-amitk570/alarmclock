e3105e97737d06bd6e39d6c66115474341266010
Amit <kumar-amitk@outlook.com>
2025-01-13 01:32:12 +0530

    Update AlarmClock.java

--- AlarmClock.java
+++ AlarmClock.java
@@ -1,13 +1,6 @@
-import java.util.Scanner;
+public class HelloWorld {
 
-public class AlarmClock {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int hours = 6;  // Default hours
-        int minutes = 0; // Default minutes
-
-        System.out.println("Running in automated mode with default alarm time: " + hours + ":" + minutes);
-        System.out.println("If manual input is required, please ensure the program is run interactively.");
+        System.out.println("Hello, World!");
     }
 }
